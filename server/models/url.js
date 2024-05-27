6101e32a77a1ddac02791712f6f59aa0b6d0ddd6
Sohail Memon <sohailmemon10@gmail.com>
2024-05-27 17:02:36 +0530

    Intializing API

---
+++ server/models/url.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const urlSchema = new mongoose.Schema({
+  urlCode: {
+    type: String,
+  },
+  longUrl: {
+    type: String,
+  },
+  shortUrl: {
+    type: String,
+  },
+  date: {
+    type: String,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.Model('Url', urlSchema);
---
+++ server/routes/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/:code', (req, res) => {});
+
+module.exports = router;
