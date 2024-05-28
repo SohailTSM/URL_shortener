476c0d798ecdccfde7f0ee2f2441f449c7f73a22
Sohail Memon <sohailmemon10@gmail.com>
2024-05-28 16:45:19 +0530

    Done with the backend part

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const Url = require('../models/url');
 
-router.get('/:code', (req, res) => {
+router.get('/:code', async (req, res) => {
   const urlCode = req.params.code;
-  res.json({ urlCode });
+  const url = await Url.findOne({ urlCode });
+  if (!url) {
+    return res.status(404).json({ message: 'No such url found' });
+  }
+  res.json({ longUrl: url.longUrl });
 });
 
 module.exports = router;
