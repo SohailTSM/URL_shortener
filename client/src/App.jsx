391a0cd72916ee5757a401358573dad3623a37f9
Sohail Memon <sohailmemon10@gmail.com>
2024-05-28 19:09:18 +0530

    Setting up frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex bg-slate-500 h-screen items-center justify-center'>
+      Test
+    </div>
+  );
+}
+
+export default App;
