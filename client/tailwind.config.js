9a325701e0ffee476d59af220cb52351fae0455b
Sohail Memon <sohailmemon10@gmail.com>
2024-05-31 18:33:53 +0530

    Done some changes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'my-blue': '#2C87C5',
+      },
+    },
   },
   plugins: [],
 };
